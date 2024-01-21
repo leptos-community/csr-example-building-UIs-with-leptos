8c4489d4c5b79b5c5b72399c85ae8b7d4034e305
Diversable <dan.mantei@outlook.com>
2024-01-20 22:33:36 -0800

    update modal component

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,3 +1,3 @@
 pub mod favourite_animal;
 pub mod incr_button;
-pub mod portal;
+pub mod modal;
--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,6 +1,6 @@
 use crate::components::favourite_animal::SelectAnimal;
 use crate::components::incr_button::IncrButton;
-use crate::components::portal::Overlay;
+use crate::components::modal::Modal;
 
 use leptos::*;
 
@@ -11,20 +11,28 @@ pub fn Home() -> impl IntoView {
         <div class="container">
 
             <picture>
-                <source srcset="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_pref_dark_RGB.svg" media="(prefers-color-scheme: dark)" />
-                <img src="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_RGB.svg" alt="Leptos Logo" height="200" width="400"/>
+                <source
+                    srcset="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_pref_dark_RGB.svg"
+                    media="(prefers-color-scheme: dark)"
+                />
+                <img
+                    src="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_RGB.svg"
+                    alt="Leptos Logo"
+                    height="200"
+                    width="400"
+                />
             </picture>
 
             <h1>"Welcome to Leptos"</h1>
 
             <div class="buttons">
-                <IncrButton />
-                <IncrButton increment=5 />
+                <IncrButton/>
+                <IncrButton increment=5/>
             </div>
 
-            <SelectAnimal />
+            <SelectAnimal/>
 
-            <Overlay />
+            <Modal/>
 
         </div>
     }
