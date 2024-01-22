d6272c1cebba98c857802bc8f77e9d4571b30e70
Diversable <dan.mantei@outlook.com>
2024-01-22 00:59:10 -0800

    can quit modal using "q" key

--- src/components/modal.rs
+++ src/components/modal.rs
@@ -1,99 +1,66 @@
-use cfg_if::cfg_if;
-use leptos::html::Div;
-// use leptos_use::use_window;
-// use leptos_use::UseDocument;
-
-// use std::convert::From;
+// use cfg_if::cfg_if;
 use std::convert::Into;
 
+use leptos::html::Div;
 use leptos::*;
 
+use wasm_bindgen::closure::Closure;
+
 use leptos_use::on_click_outside;
+use web_sys::KeyboardEvent;
 
 
-// use leptos_use::use_window;
-
-
-cfg_if! {
-    if #[cfg(not(feature = "ssr"))] {
-        // use wasm_bindgen::closure::Closure;
-        // use wasm_bindgen::JsCast;
-    }
+async fn noop(ev: KeyboardEvent) -> String {
+    logging::log!("You tried to escape the modal!");
+    logging::log!("evt: {:?}", ev);
+    "it worked".to_string()
 }
-
 
 #[component]
 pub fn Modal() -> impl IntoView {
-    // --- Set Up Modal (with click outside to dismiss behaviour) ---
-    let (show_overlay, set_show_overlay) = create_signal(false);
+    // --- Set Up Modal (with 'click outside modal to dismiss' behaviour) ---
+    let (show_modal, set_show_modal) = create_signal(false);
 
 
-    // add event listener to window & close modal when clicked outside modal window
-    cfg_if! {
-        if #[cfg(not(feature = "ssr"))] {
+    // Dismiss modal when "Escape" (or 'q') key is pressed
+    let dismiss_modal_with_keyboard = window_event_listener(ev::keypress, move |ev| {
+        if ev.key() == "Escape" || ev.key() == "q" || ev.key() == "Q" {
+            logging::log!("You tried to escape the modal!");
+            set_show_modal(false);
+        }
 
-        // let window = web_sys::window().unwrap();
-        // let window = use_window();
-        window_event_listener(ev::keypress, |ev| {
+        logging::log!("ev.key: {}", ev.key());
+        logging::log!("ev.code: {}", ev.code());
+        logging::log!("ev.key_code: {}", ev.key_code());
+    });
+    on_cleanup(move || dismiss_modal_with_keyboard.remove());
 
-            // logging::log!("{}", ev.code());
-            // if keycode == "escape".to_string() {
-            //     set_show_overlay(false);
-            // }
-            if ev.code() == "Escape".to_string().as_str() {
-                // &set_show_overlay(false);
-                logging::log!("{}", ev.code());
-            }
 
-        });
+    // use `Leptos-Use` to setup modal 'click outside modal to dismiss' behaviour
+    let modal_target: NodeRef<Div> = create_node_ref::<Div>();
+    on_cleanup(on_click_outside(modal_target, move |_| {
+        set_show_modal(false)
+    }));
 
-        // use `Leptos-Use` to setup modal 'click outside modal to dismiss' behaviour
-        let modal_target: NodeRef<Div> = create_node_ref::<Div>();
-        on_cleanup(on_click_outside(modal_target, move |_| {
-            set_show_overlay(false)
-        }));
-    }}
-
-    // --- Modal Body ---
-    let (show_inside_overlay, set_show_inside_overlay) = create_signal(false);
-    // --- Modal Body ---
+    // --- End Modal Setup ---
 
 
     view! {
-        <button id="btn-show" on:click=move |_| set_show_overlay(true)>
+        <button id="btn-show" on:click=move |_| set_show_modal(true)>
             Show Modal
         </button>
 
-        <Show when=show_overlay fallback=|| ()>
+        <Show when=show_modal fallback=|| ()>
             <Portal mount=document().get_element_by_id("app").unwrap()>
-                <div class="portal_background">
+                <div class="modal_background">
                     <div node_ref=modal_target>
-                        <dialog class="portal_content" open=show_overlay>
+                        <dialog class="modal_content" open=show_modal>
 
-                            <button id="btn-hide" on:click=move |_| set_show_overlay(false)>
+                            <button id="btn-hide" on:click=move |_| set_show_modal(false)>
                                 "Close ❌"
                             </button>
 
-                            <aside class="portal_body">
-
-                                <p>"This is in the modal's (portal) body element"</p>
-
-                                <button
-                                    id="btn-toggle"
-                                    on:click=move |_| set_show_inside_overlay(
-                                        !show_inside_overlay(),
-                                    )
-                                >
-
-                                    "Toggle modal content"
-                                </button>
-
-                                <Show when=show_inside_overlay fallback=|| view! { "Hidden" }>
-                                    "Visible"
-                                </Show>
-
-                                <p>"More contents..."</p>
-                            </aside>
+                            <ModalBody/>
 
                         </dialog>
                     </div>
@@ -102,3 +69,53 @@ pub fn Modal() -> impl IntoView {
         </Show>
     }
 }
+
+#[component]
+fn ModalBody() -> impl IntoView {
+    // --- Modal Body ---
+    let (show_inside_overlay, set_show_inside_overlay) = create_signal(false);
+    // --- End Modal Body ---
+    view! {
+        <aside class="modal_body">
+
+            <p>"This is in the modal's (portal) body"</p>
+
+            <button
+                id="btn-toggle"
+                on:click=move |_| set_show_inside_overlay(!show_inside_overlay())
+            >
+
+                "Toggle modal content"
+            </button>
+
+            <Show when=show_inside_overlay fallback=|| view! { "Hidden" }>
+                "Visible"
+            </Show>
+
+            <p>"More contents..."</p>
+        </aside>
+    }
+}
+
+
+// Option 1: handle keyboard "Escape" key press to dismiss modal
+// let window = web_sys::window().unwrap();
+
+// let handle_click = Closure::wrap(Box::new(move |event: web_sys::KeyboardEvent| {
+//     if event.key() == "Escape" {
+//         logging::log!("escape key was pressed!")
+//     }
+// }) as Box<dyn FnMut(_)>);
+
+// let handle_click = move |evt: web_sys::KeyboardEvent| {
+//     logging::log!("ev.key: {}", evt.key());
+//     if evt.key() == "Escape" {
+//         logging::log!("escape key was pressed!")
+//     }
+// };
+
+// window
+//     .add_event_listener_with_callback("keydown", handle_click(event_target(evt)))
+//     .expect("problem window evt listener");
+
+// Option 2: handle keyboard "Escape" keypress to dismiss modal
--- src/components/modal_form.rs
+++ src/components/modal_form.rs
@@ -1,10 +1,13 @@
-use cfg_if::cfg_if;
-use leptos::html::{dialog, input, Div};
+// use cfg_if::cfg_if;
+use leptos::html::Div;
 use leptos::*;
+
+use wasm_bindgen::closure::Closure;
 
 use leptos_use::on_click_outside;
 
 use regex_lite::Regex;
+use wasm_bindgen::JsCast;
 
 
 #[component]
@@ -18,13 +21,52 @@ pub fn FormModal() -> impl IntoView {
         set_show_overlay(false)
     }));
 
+    // handle keyboard "Escape" key press to dismiss modal
     let window = web_sys::window().unwrap();
 
-    // window
-    //     .add_event_listener_with_callback("click", handle_click.as_ref().unchecked_ref())
-    //     .expect("problem adding modal evt listener");
+    let handle_click = Closure::wrap(Box::new(move |event: web_sys::KeyboardEvent| {
+        if event.location() == 27 {
+            logging::log!("escape key was pressed!")
+        }
+    }) as Box<dyn FnMut(_)>);
+
+    window
+        .add_event_listener_with_callback("keydown", handle_click.as_ref().unchecked_ref())
+        .expect("problem window evt listener");
+
     // --- End Modal Setup ---
 
+
+    view! {
+        <button id="btn-show" on:click=move |_| set_show_overlay(true)>
+            "Show Form Modal"
+        </button>
+
+        <Show when=show_overlay fallback=|| ()>
+            <Portal mount=document().get_element_by_id("app").unwrap()>
+                <div class="portal_background">
+                    <div _ref=modal_target>
+                        <dialog class="portal_content" open=show_overlay>
+
+                            <div>
+                                <button id="btn-hide" on:click=move |_| set_show_overlay(false)>
+                                    "Close ❌"
+                                </button>
+
+                                <ModalBody set_show_overlay/>
+
+                            </div>
+
+                        </dialog>
+                    </div>
+                </div>
+            </Portal>
+        </Show>
+    }
+}
+
+#[component]
+fn ModalBody(set_show_overlay: WriteSignal<bool>) -> impl IntoView {
     // --- Modal Body ---
     let (show_inside_overlay, set_show_inside_overlay) = create_signal(false);
 
@@ -47,76 +89,50 @@ pub fn FormModal() -> impl IntoView {
     let (check_email, _set_check_email) = create_signal(email_is_good());
 
     view! {
-        <button id="btn-show" on:click=move |_| set_show_overlay(true)>
-            "Show Form Modal"
-        </button>
+        <aside class="portal_body">
 
-        <Show when=show_overlay fallback=|| ()>
-            <Portal mount=document().get_element_by_id("app").unwrap()>
-                <div class="portal_background">
-                    <div _ref=modal_target>
-                        <dialog class="portal_content" open=show_overlay>
+            <p>"This is in the modal's (portal) body element"</p>
 
-                            <div>
-                                <button id="btn-hide" on:click=move |_| set_show_overlay(false)>
-                                    "Close ❌"
-                                </button>
+            <button
+                id="btn-toggle"
+                on:click=move |_| set_show_inside_overlay(!show_inside_overlay())
+            >
 
-                                <aside class="portal_body">
+                "Toggle modal content"
+            </button>
 
-                                    <p>"This is in the modal's (portal) body element"</p>
+            <Show when=show_inside_overlay fallback=|| view! { "Hidden" }>
+                "Visible"
+            </Show>
 
-                                    <button
-                                        id="btn-toggle"
-                                        on:click=move |_| set_show_inside_overlay(
-                                            !show_inside_overlay(),
-                                        )
-                                    >
+            <p>"More contents..."</p>
 
-                                        "Toggle modal content"
-                                    </button>
+            <form method="dialog">
+                <label for="email">"Please enter your email address"</label>
+                <br/>
+                <input
+                    name="email"
+                    id="email"
+                    type="email"
+                    placeholder="your.email@website.com"
+                    prop:value=email_addr
+                    on:input=on_input
+                />
 
-                                    <Show when=show_inside_overlay fallback=|| view! { "Hidden" }>
-                                        "Visible"
-                                    </Show>
+                <span>
+                    {move || {
+                        if check_email() {
+                            format!(" {}", "✅".to_string())
+                        } else {
+                            format!(" {}", "❌".to_string())
+                        }
+                    }}
 
-                                    <p>"More contents..."</p>
+                </span>
 
-                                    <form method="dialog">
-                                        <label for="email">"Please enter your email address"</label>
-                                        <br/>
-                                        <input
-                                            name="email"
-                                            id="email"
-                                            type="email"
-                                            placeholder="your.email@website.com"
-                                            prop:value=email_addr
-                                            on:input=on_input
-                                        />
-
-                                        <span>
-                                            {move || {
-                                                if check_email() {
-                                                    format!(" {}", "✅".to_string())
-                                                } else {
-                                                    format!(" {}", "❌".to_string())
-                                                }
-                                            }}
-
-                                        </span>
-
-                                        <br/>
-                                        <button on:click=move |_| set_show_overlay(
-                                            false,
-                                        )>"Submit"</button>
-                                    </form>
-                                </aside>
-                            </div>
-
-                        </dialog>
-                    </div>
-                </div>
-            </Portal>
-        </Show>
+                <br/>
+                <button on:click=move |_| set_show_overlay(false)>"Submit"</button>
+            </form>
+        </aside>
     }
 }
