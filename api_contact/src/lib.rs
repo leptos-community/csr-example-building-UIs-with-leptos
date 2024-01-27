0f858b7ad9e630bc3da9a004fa6533b8c0259c73
Diversable <dan.mantei@outlook.com>
2024-01-27 14:15:32 -0800

    add contact form endpoint

---
+++ api_contact/src/lib.rs
@@ -0,0 +1,15 @@
+use spin_sdk::http::{IntoResponse, Request, Response};
+use spin_sdk::http_component;
+
+/// A simple Spin HTTP component.
+#[http_component]
+fn handle_api_contact(req: Request) -> anyhow::Result<impl IntoResponse> {
+    println!("Handling request to {:?}", req.header("spin-full-url"));
+
+
+    Ok(Response::builder()
+        .status(200)
+        .header("content-type", "text/json")
+        .body("{\"message\": \"Hello, Person\"}")
+        .build())
+}
